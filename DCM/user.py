6635cb829176a2eb12547a043afd03d553039398
Michael Nasr <nasrm4@mcmaster.ca>
2023-10-13 00:04:37 -0400

    working directory fix for file reading/writing

--- DCM/user.py
+++ DCM/user.py
@@ -1,3 +1,5 @@
+import os
+
 class User:
     def __init__(self, name, password):
         self.name = name
@@ -7,14 +9,18 @@ class Accounts:
     def __init__(self):
         self.accounts = []
         self.length = 0
-        
-        # Populate Array with Users on File
+
+        # Get the directory of the currently executing script (this is so that when Tas open the project, working directory will not matter)
+        script_directory = os.path.dirname(os.path.abspath(__file__))
+        self.accounts_file_path = os.path.join(script_directory, "accounts.txt")
+
+        # Populate Array with Users from File
         self.load_accounts()
 
     # Read from file and store accounts in object
     def load_accounts(self):
         try:
-            with open("accounts.txt", "r") as file:
+            with open(self.accounts_file_path, "r") as file:
                 lines = file.readlines()
                 for line in lines:
                     parts = line.strip().split()
@@ -25,23 +31,21 @@ class Accounts:
         except FileNotFoundError:
             # Handle the case where the file doesn't exist yet
             pass
-    
+
     # Add User to Object
     def add_user(self, username, password):
-
         # Create User
         if self.length < 10:
             self.accounts.append(User(username, password))
             self.length += 1
-
             self.update_file()
         else:
             print("Accounts limit reached (10 users).")
-    
+
     # Write on File
     def update_file(self):
         try:
-            with open("accounts.txt", "w") as file:
+            with open(self.accounts_file_path, "w") as file:
                 for user in self.accounts:
                     file.write(f"{user.name} {user.password}\n")
         except IOError as e:
